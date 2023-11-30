2290fa252e16fcde511f4e0350c2f5dd6d88b7bf
Marat <milya010790@mail.ru>
2023-11-30 23:23:21 +0500

    Создали метод EnterConsoleString для контроля ввода данных с консоли

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+var result = EnterUser();
+Console.WriteLine($"Фамилия: {result.LastName} \nИмя: {result.Name}") ;
+
+static (string Name, string LastName) EnterUser()
+{
+    (string Name, string LastName) User;
+
+    User.Name = EnterConsoleString("Введите имя: ");
+
+    User.LastName = EnterConsoleString("Введите Фамилию: ");
+
+    return User;
+}
+
+// Ввод данных с консоли
+static string EnterConsoleString(string text)
+{
+    string? result = null;
+    while (string.IsNullOrWhiteSpace(result))
+    {
+        Console.WriteLine(text);
+        result = Console.ReadLine();
+        if (string.IsNullOrWhiteSpace(result))
+        {
+            var consoleColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Вы не ввели данные");
+            Console.ForegroundColor = consoleColor;
+        }
+        else
+            break;
+    }
+   return result;
+}
+
