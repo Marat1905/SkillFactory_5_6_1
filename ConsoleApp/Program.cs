d24351d80d459bcf0edb7439dcd6bdb23126f38d
Marat <milya010790@mail.ru>
2023-11-30 23:49:00 +0500

    Создали метод EnterConsoleInt для  ввода данных с консоли и преобразования в int

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,37 +1,58 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 var result = EnterUser();
-Console.WriteLine($"Фамилия: {result.LastName} \nИмя: {result.Name}") ;
 
-static (string Name, string LastName) EnterUser()
+Console.WriteLine($"Фамилия: {result.LastName} \nИмя: {result.Name} \nВозраст: {result.Age}") ;
+
+
+static (string Name, string LastName, int Age) EnterUser()
 {
-    (string Name, string LastName) User;
+    (string Name, string LastName, int Age) User;
 
     User.Name = EnterConsoleString("Введите имя: ");
 
     User.LastName = EnterConsoleString("Введите Фамилию: ");
 
+    User.Age = EnterConsoleInt("Введите возраст цифрами: ");
     return User;
 }
 
 // Ввод данных с консоли
-static string EnterConsoleString(string text)
+static string EnterConsoleString(string text, string warningText= "Вы не ввели данные")
 {
     string? result = null;
     while (string.IsNullOrWhiteSpace(result))
     {
-        Console.WriteLine(text);
+        Console.Write(text);
         result = Console.ReadLine();
         if (string.IsNullOrWhiteSpace(result))
-        {
-            var consoleColor = Console.ForegroundColor;
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Вы не ввели данные");
-            Console.ForegroundColor = consoleColor;
-        }
+            WriteWarning(warningText);
         else
             break;
     }
    return result;
 }
 
+// Ввод данных с для преобразования в int
+static int EnterConsoleInt(string text, string warningText = "Неверный формат данных")
+{
+    int result = 0;
+    while(true)
+    {
+        var resultText = EnterConsoleString(text, warningText);
+        if (!int.TryParse(resultText, out result))
+             WriteWarning(warningText);
+        else
+            break;
+    }
+    return result;
+}
+
+// Выводим в консоль сообщение об ошибке
+static void WriteWarning(string warningText)
+{
+    var consoleColor = Console.ForegroundColor;
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(warningText);
+    Console.ForegroundColor = consoleColor;
+}
